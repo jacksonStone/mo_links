db36dac12985ae6095d89988abf0b53bdab2e919
Jackson Stone <jacksonastone@gmail.com>
2024-07-21 13:47:35 -0500

    update extension and server

--- server/server.go
+++ server/server.go
@@ -6,8 +6,11 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/____reserved/privacy_policy", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I will never look at your content. The only thing this chrome extension will do is expand mo/ to https://www.molinks.me/. Everything else is managed within https://www.molinks.me/.")
+	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "This site is a WIP, it not yet ready for public use.")
 	})
 	http.ListenAndServe(":3003", nil)
 }
