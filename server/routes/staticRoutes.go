03a1254a0530d3491826725a732f556b65586099
Jackson Stone <jacksonastone@gmail.com>
2024-08-03 11:01:28 -0500

    refactor static routes

---
+++ server/routes/staticRoutes.go
@@ -0,0 +1,72 @@
+package routes
+
+import (
+	"embed"
+	"net/http"
+	"strings"
+)
+
+//go:embed static
+var static embed.FS
+
+func InitStaticRoutes() {
+
+	http.HandleFunc("/____reserved/create_organization", serveCreateOrganizationPage)
+	http.HandleFunc("/____reserved/edit_organization", serveEditOrganizationPage)
+	http.HandleFunc("/____reserved/privacy_policy", getPrivacyPolicyEndpoint)
+	http.HandleFunc("/____reserved/login_page", loginPageEndpoint)
+	http.HandleFunc("/____reserved/static/", serveStaticFiles)
+	http.HandleFunc("/favicon.ico", faviconEndpoint)
+
+}
+func serveStaticFiles(w http.ResponseWriter, r *http.Request) {
+	if strings.HasSuffix(r.URL.Path, ".css") {
+		w.Header().Set("Content-Type", "text/css")
+	}
+	if strings.HasSuffix(r.URL.Path, ".js") {
+		w.Header().Set("Content-Type", "text/javascript")
+	}
+	if strings.HasSuffix(r.URL.Path, ".png") {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+		w.Header().Set("Content-Type", "image/png")
+	}
+	if strings.HasSuffix(r.URL.Path, ".svg") {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+		w.Header().Set("Content-Type", "image/svg+xml")
+	}
+	returnStaticFile(w, strings.TrimPrefix(r.URL.Path, "/____reserved/"))
+}
+func returnStaticFile(w http.ResponseWriter, path string) {
+	bytes, err := static.ReadFile(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(bytes)
+}
+
+func faviconEndpoint(w http.ResponseWriter, r *http.Request) {
+	// Set appropriate content type
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	returnStaticFile(w, "static/logo-16.png")
+}
+func ServeHomePage(w http.ResponseWriter) {
+	returnStaticFile(w, "static/index.html")
+}
+
+func serveCreateOrganizationPage(w http.ResponseWriter, r *http.Request) {
+	returnStaticFile(w, "static/create_organization.html")
+}
+
+func serveEditOrganizationPage(w http.ResponseWriter, r *http.Request) {
+	returnStaticFile(w, "static/edit_organization.html")
+}
+
+func getPrivacyPolicyEndpoint(w http.ResponseWriter, r *http.Request) {
+	returnStaticFile(w, "static/privacy_policy.html")
+}
+
+func loginPageEndpoint(w http.ResponseWriter, r *http.Request) {
+	returnStaticFile(w, "static/login.html")
+}
--- server/server.go
+++ server/server.go
@@ -3,7 +3,6 @@ package main
 // TODO Leverage organization endpoints (Still need to be able to add people)
 
 import (
-	"embed"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -18,9 +17,6 @@ import (
 	"strings"
 	"time"
 )
-
-//go:embed static
-var static embed.FS
 
 type AddLinkRequest struct {
 	Url  string `json:"url"`
@@ -61,13 +57,7 @@ func main() {
 	http.HandleFunc("/____reserved/api/organization/assign_member", assignMemberEndpoint)
 	http.HandleFunc("/____reserved/api/organization/members", getOrganizationMembersEndpoint)
 
-	http.HandleFunc("/____reserved/create_organization", serveCreateOrganizationPage)
-	http.HandleFunc("/____reserved/edit_organization", serveEditOrganizationPage)
-	http.HandleFunc("/____reserved/privacy_policy", getPrivacyPolicyEndpoint)
-	http.HandleFunc("/____reserved/login_page", loginPageEndpoint)
-
-	http.HandleFunc("/____reserved/static/", serveStaticFiles)
-	http.HandleFunc("/favicon.ico", faviconEndpoint)
+	routes.InitStaticRoutes()
 
 	http.HandleFunc("/", handleAttemptedMoLink)
 
@@ -76,57 +66,7 @@ func main() {
 		log.Fatal(err)
 	}
 }
-func serveStaticFiles(w http.ResponseWriter, r *http.Request) {
-	if strings.HasSuffix(r.URL.Path, ".css") {
-		w.Header().Set("Content-Type", "text/css")
-	}
-	if strings.HasSuffix(r.URL.Path, ".js") {
-		w.Header().Set("Content-Type", "text/javascript")
-	}
-	if strings.HasSuffix(r.URL.Path, ".png") {
-		w.Header().Set("Cache-Control", "public, max-age=31536000")
-		w.Header().Set("Content-Type", "image/png")
-	}
-	if strings.HasSuffix(r.URL.Path, ".svg") {
-		w.Header().Set("Cache-Control", "public, max-age=31536000")
-		w.Header().Set("Content-Type", "image/svg+xml")
-	}
-	returnStaticFile(w, strings.TrimPrefix(r.URL.Path, "/____reserved/"))
-}
-func returnStaticFile(w http.ResponseWriter, path string) {
-	bytes, err := static.ReadFile(path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Write(bytes)
-}
 
-func faviconEndpoint(w http.ResponseWriter, r *http.Request) {
-	// Set appropriate content type
-	w.Header().Set("Content-Type", "image/png")
-	w.Header().Set("Cache-Control", "public, max-age=31536000")
-	returnStaticFile(w, "static/logo-16.png")
-}
-func serveHomePage(w http.ResponseWriter) {
-	returnStaticFile(w, "static/index.html")
-}
-
-func serveCreateOrganizationPage(w http.ResponseWriter, r *http.Request) {
-	returnStaticFile(w, "static/create_organization.html")
-}
-
-func serveEditOrganizationPage(w http.ResponseWriter, r *http.Request) {
-	returnStaticFile(w, "static/edit_organization.html")
-}
-
-func getPrivacyPolicyEndpoint(w http.ResponseWriter, r *http.Request) {
-	returnStaticFile(w, "static/privacy_policy.html")
-}
-
-func loginPageEndpoint(w http.ResponseWriter, r *http.Request) {
-	returnStaticFile(w, "static/login.html")
-}
 func meEndpoint(w http.ResponseWriter, r *http.Request) {
 	user, err := auth.GetAuthenticatedUser(r)
 	if err != nil {
@@ -198,7 +138,7 @@ func handleAttemptedMoLink(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if r.URL.Path == "/" {
-		serveHomePage(w)
+		routes.ServeHomePage(w)
 		return
 	}
 	user, err := auth.GetAuthenticatedUser(r)
@@ -217,7 +157,7 @@ func handleAttemptedMoLink(w http.ResponseWriter, r *http.Request) {
 	}
 	if len(links) == 0 {
 		// TODO Open up the home page to create a new link
-		serveHomePage(w)
+		routes.ServeHomePage(w)
 		return
 	}
 	link := links[0]
